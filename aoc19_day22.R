689714f189520a616723f9be75e5b24555768cd0
Maarten Demeyer <maarten@mpj-analytics.co.uk>
2019-12-22 23:08:27 +0100

    Day 22

--- aoc19_day22.R
+++ aoc19_day22.R
@@ -10,40 +10,40 @@ inp <- readLines("input/input22.txt")
 
 ## -- PART 1 --
 into_new <- function(cards) {
-  rev(cards)
+    rev(cards)
 }
 
 cut_n <- function(cards, n) {
-  if (n < 0) n <- length(cards) + n
-  c(cards[(n + 1):length(cards)], cards[1:n])
+    if (n < 0) n <- length(cards) + n
+    c(cards[(n + 1):length(cards)], cards[1:n])
 }
 
 increment_n <- function(cards, n) {
-  nc <- length(cards)
-  new_pos <- 1 + (seq(0, (nc * n) - 1, by = n) %% nc)
-  cards[order(new_pos)]
+    nc <- length(cards)
+    new_pos <- 1 + (seq(0, (nc * n) - 1, by = n) %% nc)
+    cards[order(new_pos)]
 }
 
 shuffle_cards <- function(n_cards, steps) {
 
-  cards <- seq(n_cards) - 1
+    cards <- seq(n_cards) - 1
 
-  for (step in steps) {
+    for (step in steps) {
 
-    idx <- regexpr("[0-9\\-]+", step)
-    n <- if (idx != -1) as.numeric(substr(step, idx, nchar(step))) else NULL
+        idx <- regexpr("[0-9\\-]+", step)
+        n <- if (idx != -1) as.numeric(substr(step, idx, nchar(step))) else NULL
 
-    cards <- if (grepl("into new", step)) {
-      into_new(cards)
-    } else if (grepl("cut", step)) {
-      cut_n(cards, n)
-    } else if (grepl("increment", step)) {
-      increment_n(cards, n)
+        cards <- if (grepl("into new", step)) {
+                     into_new(cards)
+                 } else if (grepl("cut", step)) {
+                     cut_n(cards, n)
+                 } else if (grepl("increment", step)) {
+                     increment_n(cards, n)
+                 }
+
     }
 
-  }
-
-  cards
+    cards
 
 }
 
@@ -66,29 +66,29 @@ n_reps <- 101741582076661
 a <- 1
 b <- 0
 for (step in rev(inp)) {
-  idx <- regexpr("[0-9\\-]+", step)
-  n <- if (idx != -1) as.numeric(substr(step, idx, nchar(step))) else NULL
-  if (grepl("into new", step)) {
-    b <- b + 1
-    a <- a * -1
-    b <- b * -1
-  } else if (grepl("cut", step)) {
-    b <- b + n
-  } else if (grepl("increment", step)) {
-    p <- gmp::powm(n, n_cards - 2, n_cards)
-    a <- a * p
-    b <- b * p
-  }
-  a <- a %% n_cards
-  b <- b %% n_cards
+    idx <- regexpr("[0-9\\-]+", step)
+    n <- if (idx != -1) as.numeric(substr(step, idx, nchar(step))) else NULL
+    if (grepl("into new", step)) {
+        b <- b + 1
+        a <- a * -1
+        b <- b * -1
+    } else if (grepl("cut", step)) {
+        b <- b + n
+    } else if (grepl("increment", step)) {
+        p <- gmp::powm(n, n_cards - 2, n_cards)
+        a <- a * p
+        b <- b * p
+    }
+    a <- a %% n_cards
+    b <- b %% n_cards
 }
 
 ## Fancy mathy simplification can tell us what would happen after n_reps of shuffles
 solution_2 <-
-  (
-    (gmp::powm(a, n_reps, n_cards) * 2020) +
-    (b * (gmp::powm(a, n_reps, n_cards) + n_cards - 1) *
+    (
+        (gmp::powm(a, n_reps, n_cards) * 2020) +
+        (b * (gmp::powm(a, n_reps, n_cards) + n_cards - 1) *
          (gmp::powm(a - 1, n_cards - 2, n_cards)))
-  ) %% n_cards
+    ) %% n_cards
 
 cat("Solution to Part 2:", format(solution_2, scientific = FALSE), "\n")
